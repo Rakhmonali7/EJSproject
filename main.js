aa92ba601d711f9a4fa72ec3dbaf411b445711f5
Rakhmonali7 <zamonalievr7@gmail.com>
2023-12-21 21:24:41 +0900

    authentification

--- main.js
+++ main.js
@@ -1,31 +1,75 @@
 "use-strict";
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0,
-      close: 24,
-    },
-  },
-};
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+class Authintification {
+  constructor() {
+    this.loggedInUser = null;
+    this.baseURL = "https://greenshop.abduvoitov.com/api";
+  }
 
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(name, hours, tags);
+  async signIn(username, password) {
+    try {
+      const response = await fetch(`${this.baseURL}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        this.loggedInUser = data.username;
+        return { message: "Sign-in successful", user: this.loggedInUser };
+      } else {
+        throw new Error("Sign-in failed");
+      }
+    } catch (error) {
+      throw new Error("Sign-in failed");
+    }
+  }
+
+  async signOut() {
+    try {
+      const response = await fetch(`${this.baseURL}/logout`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        this.loggedInUser = null;
+        return { message: "Sign-out successful" };
+      } else {
+        throw new Error("Sign-out failed");
+      }
+    } catch (error) {
+      throw new Error("Sign-out failed");
+    }
+  }
+
+  getCurrentUser() {
+    return this.loggedInUser;
+  }
+}
+
+const fakeAPI = new Authintification();
+
+fakeAPI
+  .signIn("user", "password")
+  .then((response) => {
+    console.log(response);
+    console.log("Logged in as:", fakeAPI.getCurrentUser());
+
+    fakeAPI
+      .signOut()
+      .then((response) => {
+        console.log(response);
+        console.log("Logged in as:", fakeAPI.getCurrentUser());
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
