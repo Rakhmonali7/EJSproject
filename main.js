18b603339bc0fed0a39e26e7c1ee77f27bfa9aac
Rakhmonali7 <zamonalievr7@gmail.com>
2024-01-17 22:40:32 +0900

    creting vrud

--- main.js
+++ main.js
@@ -1,58 +1,29 @@
 "use-strict";
+const http = require("http");
 
-class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = this.right = null;
+const user = [
+  {
+    name: "Asadbek",
+    surname: "Absuvoitov",
+    id: 1,
+  },
+];
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/" && method === "GET") {
+    res.writeHead(200);
+    return res.end("Welcome to the first server created with nodejs");
+  } else if (url === "/user" && method === "GET") {
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    return res.end(JSON.stringify(user));
   }
-}
+});
 
-function sortedArrayToBST(nums) {
-  if (nums.length === 0) {
-    return null;
-  }
-
-  const mid = Math.floor(nums.length / 2);
-  const root = new TreeNode(nums[mid]);
-
-  root.left = sortedArrayToBST(nums.slice(0, mid));
-  root.right = sortedArrayToBST(nums.slice(mid + 1));
-
-  return root;
-}
-class BinarySearch {
-  constructor(sortedArray) {
-    this.array = sortedArray;
-  }
-
-  search(target) {
-    let left = 0;
-    let right = this.array.length - 1;
-
-    while (left <= right) {
-      const mid = Math.floor((left + right) / 2);
-
-      if (this.array[mid] === target) {
-        return mid; // Found the target
-      } else if (this.array[mid] < target) {
-        left = mid + 1; // Search in the right half
-      } else {
-        right = mid - 1; // Search in the left half
-      }
-    }
-
-    return -1; // Target not found
-  }
-}
-
-// Example usage:
-const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const binarySearchInstance = new BinarySearch(sortedArray);
-const targetValue = 6;
-const result = binarySearchInstance.search(targetValue);
-
-if (result !== -1) {
-  console.log(`${targetValue} found at index ${result}.`);
-} else {
-  console.log(`${targetValue} not found in the array.`);
-}
+server.listen(3000, () => {
+  console.log(`Your server is running on: http://localhost:3000`);
+});
