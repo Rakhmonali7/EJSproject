b39d1ff022125737fd77b5ea4d207b0c290481a2
Rakhmonali7 <zamonalievr7@gmail.com>
2024-01-21 20:07:24 +0900

    clean 1

--- main.js
+++ main.js
@@ -1,29 +1,15 @@
 "use-strict";
-const http = require("http");
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
 
-const user = [
-  {
-    name: "Asadbek",
-    surname: "Absuvoitov",
-    id: 1,
-  },
-];
+Object.prototype.count = function(){
+  return Object.values(this).reduce((cur, accum) => {
+    accum += cur
+    return accum
+  }, 0)
+}
+console.log(salaries.count())
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/" && method === "GET") {
-    res.writeHead(200);
-    return res.end("Welcome to the first server created with nodejs");
-  } else if (url === "/user" && method === "GET") {
-    res.writeHead(200, {
-      "content-type": "application/json",
-    });
-    return res.end(JSON.stringify(user));
-  }
-});
-
-server.listen(3000, () => {
-  console.log(`Your server is running on: http://localhost:3000`);
-});
