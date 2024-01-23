eab8dc88de65133b072008a2a1af52723731f697
Rakhmonali7 <zamonalievr7@gmail.com>
2024-01-23 18:06:56 +0900

    EJS updating code making component based

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -7,6 +8,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
+// Middleware to connect css
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.status(200).render('index', { pageTitle: 'Home Page' });
@@ -16,6 +19,13 @@ app.get('/about', (req, res) => {
   res.status(200).render('about', { pageTitle: 'About Page' });
 });
 
+app.post('/', (req, res)=>{
+
+  res.status(201).send('post successful')
+})
+
 app.listen(3000, () => {
   console.log('This app is running at http://localhost:3000');
 });
+
+
